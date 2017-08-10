2c1b1d52fc825d9e4fa0ef4027767226d15119a7
Manu Mitraan K <manumitraan@gmail.com>
2017-08-10 09:06:24 +0530

    Finished Auto login to Kerberos
    Done the basic implementation of saving password and logging in. Need to
    make it more beautiful

--- popup.js
+++ popup.js
@@ -1,20 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+window.onload = function() {
+  document.getElementById('save').onclick = function() {
+    var username = document.getElementById('username').value;
+    var pass = document.getElementById('password').value;
+    var auto = document.getElementById('autologin').checked;
+    chrome.storage.sync.set({'username': username},function() {});
+    chrome.storage.sync.set({'password': pass},function() {}); 
+    chrome.storage.sync.set({'auto': auto},function() {}); 
+  }
+}
